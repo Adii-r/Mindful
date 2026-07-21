db57133bdd0cc015243d31865208203d1b0bd533
Adii <adityaraut9b@gmail.com>
2026-07-21 21:25:42 +0545

    feat(onboarding): completed onboarding module

---
+++ mindful/lib/features/onboarding/data/models/category_model.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CategoryModel {
+  final String id;
+  final String name;
+  final String icon;
+
+  CategoryModel({
+    required this.id,
+    required this.name,
+    required this.icon,
+  });
+
+  factory CategoryModel.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+
+    return CategoryModel(
+      id: doc.id,
+      name: data['name'] ?? '',
+      icon: data['icon'] ?? '',
+    );
+  }
+}
