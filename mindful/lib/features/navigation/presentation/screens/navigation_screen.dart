d898a6ed84b29039e45afae262a2bdc7f879c81b
Adii <adityaraut9b@gmail.com>
2026-07-21 21:59:01 +0545

    bottom niavgation bar configured

---
+++ mindful/lib/features/navigation/presentation/screens/navigation_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+
+
+class NavigationScreen extends StatefulWidget {
+  const NavigationScreen({super.key});
+
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen> {
+
+  int currentIndex = 0;
+
+  final pages = const [
+    //HomeScreen(),
+    //ChatScreen(),
+    //GameScreen(),
+    //FavoritesScreen(),
+    //ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: pages[currentIndex],
+
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentIndex,
+
+        onDestinationSelected: (index){
+          setState(() {
+            currentIndex = index;
+          });
+        },
+
+        destinations: const [
+
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+
+          NavigationDestination(
+            icon: Icon(Icons.chat_bubble_outline),
+            selectedIcon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+
+          NavigationDestination(
+            icon: Icon(Icons.sports_esports_outlined),
+            selectedIcon: Icon(Icons.sports_esports),
+            label: 'Games',
+          ),
+
+          NavigationDestination(
+            icon: Icon(Icons.favorite_border),
+            selectedIcon: Icon(Icons.favorite),
+            label: 'Saved',
+          ),
+
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            selectedIcon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
